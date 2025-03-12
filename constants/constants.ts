c62f653d292f4cdf03955faf0a8b06b4b975e540
Kevin Qu <kevinqu88@gmail.com>
2025-03-12 00:29:59 -0400

    env variable name updated

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,3 +1,3 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
-export const appName = isProduction ? process.env.APP_NAME : process.env.NEXT_PUBLIC_APP_NAME;
+export const appName = isProduction ? process.env.NEXT_PUBLIC_APP_NAME : process.env.NEXT_PUBLIC_APP_NAME;
